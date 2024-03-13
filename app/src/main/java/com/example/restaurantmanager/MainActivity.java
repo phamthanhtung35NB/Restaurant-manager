59fcb4e3b3952a2f93813b4042dbc6dfef21bdf3
Phạm Thanh Tùng <tungpham010203@gmail.com>
2024-03-14 03:33:57 +0700

    update so khai

--- app/src/main/java/com/example/restaurantmanager/MainActivity.java
+++ app/src/main/java/com/example/restaurantmanager/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.restaurantmanager;
 
+import android.health.connect.datatypes.SleepSessionRecord;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +15,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textViewLocation;
+    ImageButton imageButtonLocation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,10 +24,18 @@ public class MainActivity extends AppCompatActivity {
         //setContentView truyền layout vào
         //R là lớp tài nguyên, layout là thư mục chứa layout, activity_main là file layout
         setContentView(R.layout.activity_main);
+        textViewLocation = this.<TextView>findViewById(R.id.textViewLocation);
+        imageButtonLocation = this.<ImageButton>findViewById(R.id.imageButtonLocation);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+    }
+    public void getLocation(View v) {
+        //code lấy vị trí
+        Toast.makeText(this, "Đang lấy vị trí", Toast.LENGTH_SHORT).show();
+        textViewLocation.setText("Hà Nội");
     }
 }
