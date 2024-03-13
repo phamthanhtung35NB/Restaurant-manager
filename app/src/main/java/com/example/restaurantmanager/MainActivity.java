c3e81240c4a9d4c7c32cadfe70b399d1e61ee87e
Phạm Thanh Tùng <tungpham010203@gmail.com>
2024-03-13 23:27:51 +0700

    khoi tao

---
+++ app/src/main/java/com/example/restaurantmanager/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.restaurantmanager;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        //setContentView truyền layout vào
+        //R là lớp tài nguyên, layout là thư mục chứa layout, activity_main là file layout
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
