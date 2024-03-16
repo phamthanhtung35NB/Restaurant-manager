5a9e0e58d9c7ac38435fd0bd6facba791bcd1f53
Phạm Thanh Tùng <tungpham010203@gmail.com>
2024-03-16 17:45:38 +0700

    connect firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.androidApplication)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -38,6 +39,8 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.play.services.maps)
     implementation("com.squareup.picasso:picasso:2.8")
+    implementation(libs.firebase.database)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/example/restaurantmanager/MainActivity.java
+++ app/src/main/java/com/example/restaurantmanager/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.restaurantmanager;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
-import android.view.Menu;
+import android.util.Log;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,12 +14,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
 import java.util.ArrayList;
 
 import adapter.MenuAdapter;
 import model.MenuRestaurant;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView1;
+    Button button1;
 
     ListView listViewMenu;
     ArrayList<MenuRestaurant> data;
@@ -40,9 +54,46 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void addEvents() {
+
+
+    }
+
+    private void onClickReadData() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+//        myRef.setValue("Hello, World!");
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+                textView1.setText(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
     }
 
     private void addControls() {
+//        // Write a message to the database
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("message");
+//
+//        myRef.setValue("Hello, World!");
+
+
+        textView1 = findViewById(R.id.textView1);
+        button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(v -> {
+            onClickReadData();
+        });
         listViewMenu = findViewById(R.id.listViewMenu);
         data = new ArrayList<>();
         data.add(new MenuRestaurant("1", "Cơm chiên", "Cơm chiên + trứng", 50000, "https://i.imgur.com/ikbFUzX.png"));
